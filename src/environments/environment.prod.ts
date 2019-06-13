eb12ec647f7d4823b514dcb5aef367151d042c8d
Ofek Ashery <ofek.ashery@gmail.com>
2019-06-13 13:27:00 +0300

    Upload environment files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  teamNumber: 1937, // Your Team Number
+  teamName: 'Elysium', // Your Team Name
+  program: 'FRC', // Your team program (FRC or FTC)
+  event_key: '', // TBA or TOA event key
+  auth_key: '' // TBA or TOA api key
+};
